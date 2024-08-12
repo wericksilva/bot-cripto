b1db00f386b89c3765eb574182c144b87be740eb
C1281964 Werick Silva Rodrigues Cruz <wericksilva-@hotmail.com>
2024-08-12 11:10:49 -0300

    fist commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+const SYMBOL = "SOLUSDT";  //"BTCUSDT";
+const BUY_PRICE = 14098;
+const SELL_PRICE = 14758;
+
+const API_URL = "https://testnet.binance.vision";
+
+//prodrução //https://api.binance.com
+
+let isOpened = false;
+
+async function start() {
+    const { data } = await axios.get(API_URL + "/api/v3/klines?limit=21&interval=15m&symbol=" + SYMBOL);
+    const candle = data[data.length - 1];
+    const price = parseFloat(candle[4]);
+
+    //console.clear();
+    console.log("Price: " + price);
+
+    
+    if (price <= BUY_PRICE && isOpened === false) {
+        isOpened = true;
+        console.log("comprar");
+    }
+    else if (price >= SELL_PRICE && isOpened === true) {
+        console.log("vender");
+        isOpened = false;
+    }
+    else{
+        console.log("aguardar");
+    }
+    
+}
+
+setInterval(start, 3000);
+
+start();
